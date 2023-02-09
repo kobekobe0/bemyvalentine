eac48567c137b94579bb9e2266b411be6a4955a9
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2023-02-10 01:41:56 +0800

    some changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
-import ConfigurePage from './ConfigurePage.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
